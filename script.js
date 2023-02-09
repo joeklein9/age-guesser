e3a66ba1ed483254731dbef8a0b96fcf24ac3bf4
Joseph Kleinhenz <115609619+joeklein9@users.noreply.github.com>
2023-02-08 23:19:03 -0500

    done

--- script.js
+++ script.js
@@ -12,14 +12,17 @@ function getResults() {
 
 fetch (`https://api.agify.io?name=${inputField.value}`)
     .then (response => response.json())
-    .then (data => 
-        
+    .then (data => {
+    
+        if (data.age !==null) {
         resultsDisplay.innerHTML = `Your age is likely: ${data.age}`
+        }
         
         
-        )
+    })
 
 
+    inputField.value = ""
     
 }
 
