4ec76ca0ffb1c106f6434f4ddeee697ceb496ccd
Joseph Kleinhenz <115609619+joeklein9@users.noreply.github.com>
2023-02-08 21:07:50 -0500

    finished

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
-fetch ("https://api.agify.io?name=michael")
+const submitBtn = document.getElementById("submit")
+const inputField = document.getElementById("input")
+let resultsDisplay = document.getElementById("results")
+
+
+
+submitBtn.addEventListener("click", getResults)
+
+
+
+function getResults() {
+
+fetch (`https://api.agify.io?name=${inputField.value}`)
     .then (response => response.json())
-    .then (data => console.log())
+    .then (data => 
+        
+        resultsDisplay.innerHTML = data.age
+        
+        
+        )
+
+
+    
+}
+
+
+
+
+
+
